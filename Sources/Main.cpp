4e44beed2c56b208c8ade8b3155513319312314e
Bogdan Bessonov <b_bessonov@wargaming.net>
2019-06-14 18:28:55 +0300

    + added color to vertices

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -42,7 +42,14 @@ using namespace DirectX;
 
 struct Vertex
 {
+	Vertex(float x, float y, float z, float r, float g, float b, float a) :
+		pos(x, y, z),
+		color(r, g, b, a)
+	{
+
+	}
 	XMFLOAT3 pos;
+	XMFLOAT4 color;
 };
 
 
@@ -257,7 +264,8 @@ bool InitD3D()
 
 	D3D12_INPUT_ELEMENT_DESC inputLayout[] =
 	{
-		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
 	};
 
 	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc = {};
@@ -294,9 +302,9 @@ bool InitD3D()
 	// Triangle
 	Vertex vList[] =
 	{
-		{ {0.0f, 0.5f, 0.5f} },
-		{ {0.5f, -0.5f, 0.5f} },
-		{ {-0.5f, -0.5f, 0.5f} },
+		{0.0f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f},
+		{0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f},
+		{-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f},
 	};
 
 	int vBufferSize = sizeof(vList);
