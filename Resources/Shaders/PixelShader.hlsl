626349d3f91b3f9c1c6bf0d60b7bcd6411d629bc
Bogdan Bessonov <b_bessonov@wargaming.net>
2019-10-03 13:54:11 +0300

    - changed spaces to tabs in shaders

--- Resources/Shaders/PixelShader.hlsl
+++ Resources/Shaders/PixelShader.hlsl
@@ -1,10 +1,10 @@
 struct PixelShaderInput
 {
-    float4 normal : NORMAL;
-    float2 uv0 : TEXCOORD;
+	float4 normal : NORMAL;
+	float2 uv0 : TEXCOORD;
 };
 
 float4 main(PixelShaderInput input) : SV_Target
 {
-    return input.normal;
+	return input.normal;
 }
--- Resources/Shaders/VertexShader.hlsl
+++ Resources/Shaders/VertexShader.hlsl
@@ -10,16 +10,16 @@ ConstantBuffer<Mat> MatCB : register(b0);
 struct VSInput
 {
 	float3 position : POSITION;
-    float3 normal : NORMAL;
-    float3 tangent : TANGENT;
-    float3 bitangent : BITANGENT;
-    float2 uv0 : TEXCOORD;
+	float3 normal : NORMAL;
+	float3 tangent : TANGENT;
+	float3 bitangent : BITANGENT;
+	float2 uv0 : TEXCOORD;
 };
 
 struct VSOutput
 {
-    float4 normal : NORMAL;
-    float2 uv0 : TEXCOORD;
+	float4 normal : NORMAL;
+	float2 uv0 : TEXCOORD;
 	float4 position : SV_Position;
 };
 
@@ -28,8 +28,8 @@ VSOutput main(VSInput input)
 	VSOutput output;
 
 	output.position = mul(MatCB.mvpMatrix, float4(input.position, 1.0f));
-    output.normal = mul(MatCB.mvpMatrix, float4(input.normal, 1.0f));
-    output.uv0 = input.uv0;
+	output.normal = mul(MatCB.mvpMatrix, float4(input.normal, 1.0f));
+	output.uv0 = input.uv0;
 
 	return output;
 }
