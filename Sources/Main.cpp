d4f2465c0a6f5e4688de9f71a00fccbc8bcee1d1
Bogdan Bessonov <b2soft@yandex.ru>
2019-11-06 18:49:31 +0300

    + added live objects reporting on exit - stabilized Release configuration

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -1,21 +1,24 @@
 #include "Precompiled.h"
 //
 #include <Shlwapi.h>
-//#include <dxgidebug.h>
+
 //
 //#include <Application.h>
 //#include "b2Game.h"
 //
-//void ReportLiveObjects()
-//{
-//#if defined(_DEBUG)
-//	IDXGIDebug1* dxgiDebug;
-//	DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug));
-//
-//	dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_IGNORE_INTERNAL);
-//	dxgiDebug->Release();
-//#endif
-//}
+
+#include <dxgidebug.h>
+#include <dxgi1_3.h>
+void ReportLiveObjects()
+{
+#if defined(_DEBUG)
+	IDXGIDebug1* dxgiDebug;
+	DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug));
+
+	dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_IGNORE_INTERNAL);
+	dxgiDebug->Release();
+#endif
+}
 
 #include "SampleApp.h"
 
@@ -33,7 +36,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 		SetCurrentDirectoryW(path);
 	}
 
-	//atexit(&ReportLiveObjects);
+	atexit(&ReportLiveObjects);
 
 	int returnCode = 0;
 	alexis::Core::Create(hInstance);
